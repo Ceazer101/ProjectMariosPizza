8c8921a4a3e8d9781406661c53293fd782a2bf65
chris <chri402s@stud.kea.dk>
2021-11-02 11:42:52 +0100

    Pizza class added. constructor, getter and ToString

--- src/com/company/Pizza.java
+++ src/com/company/Pizza.java
@@ -1,4 +1,35 @@
 package com.company;
 
 public class Pizza {
+    private int number;
+    private String name;
+    private String ingridiens;
+    private int price;
+
+    public Pizza (int number, String name, String ingridiens, int price){
+        this.name = name;
+        this.ingridiens = ingridiens;
+        this.price = price;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIngridiens() {
+        return ingridiens;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return number + ". " + name + ", " + ingridiens + ": " + price;
+    }
 }
