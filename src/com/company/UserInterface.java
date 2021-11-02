77235cfb720c3559f570d89cab4ff9559bd51400
majamatboue <maja962h@stud.kea.dk>
2021-11-02 11:45:40 +0100

    så vi igang

--- src/com/company/UserInterface.java
+++ src/com/company/UserInterface.java
@@ -1,4 +1,50 @@
 package com.company;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class UserInterface {
+
+    private Scanner scanner = new Scanner(System.in);
+    private String input;
+
+
+    public void userOptions() {
+        System.out.println("Du har følgende valgmuligheder:");
+        System.out.println("Du kan tatste følgende kommandoer: ");
+        System.out.println("'o' for at oprette ordre\n'm' for at se menu\n's' for at se bestillingsoversigt" +
+                "\n'a' for at afslutte en ordre\n'g' for at gemme ordrehistorik");
+
+
+        while (true) {
+            input = scanner.next().trim().toLowerCase(Locale.ROOT);
+            switch(input) {
+                case "opret", "o":
+
+                    break;
+
+                case "menu", "m":
+
+                    break;
+
+                case "se", "s":
+
+                    break;
+
+                case "afslut", "a":
+
+                    break;
+
+                case "gem", "g":
+
+                    break;
+
+                default:
+                    System.out.println("Det forsod vi ikke helt, prøv at indtast en ny kommando.");
+                    break;
+            }
+        }
+    }
+
+
+
 }
