e400cca323f69aac1db72b7e23a39be3cb639bf1
guler <gule0153@stud.kea.dk>
2021-11-06 11:28:52 +0100

    added comments to Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,16 @@
 package com.company;
 
-import java.io.IOException;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
+        //Initializing a UserInterface object.
         UserInterface ui = new UserInterface();
+
+        //method call.
         ui.userOptions();
+
+        //Linebreak for aesthetic.
         System.out.println();
 
     }
