66660e386de23a48e0b5d796634936ce7ca66142
guler <gule0153@stud.kea.dk>
2021-11-06 11:33:28 +0100

    added comments to UserInterface class

--- src/com/company/UserInterface.java
+++ src/com/company/UserInterface.java
@@ -1,15 +1,15 @@
 package com.company;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class UserInterface {
 
+    //Attributes.
     private Scanner scanner = new Scanner(System.in);
     private String input;
     private Controller controller;
 
-
+    //Constructor.
     public UserInterface() {
         this.controller = new Controller();
     }
@@ -17,20 +17,27 @@ public class UserInterface {
 
     public void userOptions() {
 
+        //Initializing variable.
         boolean running = true;
+
         while (running) {
+
+            //Prints the different commands the user can write.
             System.out.println();
             System.out.println("Du kan taste følgende kommandoer: ");
             System.out.println("'o' for at oprette en bestilling\n'r' for at rette en bestilling\n'g' for at gemme alle aktive bestillinger" +
                     "\n'a' for at afslutte programmet\n's' for at se bestillingsoversigt\n'm' for at se menuen\n");
+
+            //Saves the userInput in variable.
             input = scanner.next().trim().toLowerCase();
+
             switch(input) {
                 case "opret", "o":
                     orderInterface();
                     break;
 
                 case "ret" , "r":
-                    //TODO: laves kun hvis der er tid
+                    //TODO: make edit function if there is time
                     break;
 
                 case "gem", "g":
@@ -65,6 +72,7 @@ public class UserInterface {
        //asks the user for pizza name
        System.out.println("indtast venligst nummer på den pizza der skal tilføjes (Slut bestilling med '0'): ");
 
+       //creates order.
        controller.createOrder(scanner);
 
    }
